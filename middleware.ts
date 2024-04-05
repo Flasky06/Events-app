ab57567268b1c8ac8a80a97eb9ffbffd3cb19dfa
flasky06 <bonnienjuguna106@gmail.com>
2024-04-05 12:24:17 +0300

    middleware updates

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export default authMiddleware({
     "/events/webhook/clerk",
     "/api/webhook/stripe",
     "api/uploadthing",
+    "api/webhook/clerk",
   ],
   ignoredRoutes: [
     "/events/webhook/clerk",
