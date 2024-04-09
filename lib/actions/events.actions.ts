ddffef0e45ac925f2dd5c08354fe3a8d5262dc3e
flasky06 <bonnienjuguna106@gmail.com>
2024-04-09 18:37:07 +0300

    completed events details page

--- lib/actions/events.actions.ts
+++ lib/actions/events.actions.ts
@@ -5,6 +5,22 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
+import { HandPlatterIcon } from "lucide-react";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({
+      path: "category",
+      model: Category,
+      select: "_id ,name",
+    });
+};
 
 // CREATE
 export async function createEvent({ userId, event, path }: CreateEventParams) {
@@ -21,7 +37,9 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
 
     console.log("organizer", organizer);
 
-    if (!organizer) throw new Error("Organizer not found");
+    if (!organizer) {
+      throw new Error("Organizer not found");
+    }
 
     console.log({ categoryId: event.categoryId, organizerId: userId });
 
@@ -39,3 +57,18 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     handleError(error);
   }
 }
+
+export const getEventsById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+};
