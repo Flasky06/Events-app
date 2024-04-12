69a1e40ad00fd085bf350414ebeb3fcc674d8714
flasky06 <bonnienjuguna106@gmail.com>
2024-04-12 09:24:21 +0300

    worked on my profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,9 +1,60 @@
+import { Button } from "@/components/ui/button";
+import Collection from "@/components/ui/shared/Collection";
+import { getEventsByUser } from "@/lib/actions/events.actions";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 });
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10"></section>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left ">My Tickets</h3>{" "}
+          <Button asChild className="button hidden sm:flex">
+            <Link href="/#events">Explore More Events</Link>
+          </Button>
+        </div>
+      </section>
+      <section className="wrapper my-8">
+        {/* <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No events tickets yet"
+          emptyStateSubtext="No worries - plenty of events to explore !"
+          collectionType="My_Tickets"
+          limit={3}
+          page={1}
+          totalPages={2}
+          urlParamName="odersPage"
+        /> */}
+      </section>
+
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left ">
+            Events Organised
+          </h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">Create New Events</Link>
+          </Button>
+        </div>
+      </section>
+      <section className="wrapper my-8">
+        <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No events have been created yet"
+          emptyStateSubtext="Go create some now"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          totalPages={2}
+          urlParamName="eventsPage"
+        />
+      </section>
     </>
   );
 };
